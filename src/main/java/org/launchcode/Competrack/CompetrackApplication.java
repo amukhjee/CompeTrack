591dce890e92348eb747af7283fa550238805e07
amukhjee <amukhjee1@gmail.com>
2020-04-24 00:26:20 -0500

    intial setup with hello world message

---
+++ src/main/java/org/launchcode/Competrack/CompetrackApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Competrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/Competrack/CompetrackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Competrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
