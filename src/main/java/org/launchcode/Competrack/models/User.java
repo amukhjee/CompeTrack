2a3e59e98a62f046434883ec258f34db5d583d7c
amukhjee <amukhjee1@gmail.com>
2020-05-14 18:43:37 -0500

    WIP for creating login using Spring Security

--- src/main/java/org/launchcode/Competrack/models/User.java
+++ src/main/java/org/launchcode/Competrack/models/User.java
@@ -1,43 +1,132 @@
 package org.launchcode.Competrack.models;
 
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Set;
+
+import javax.management.relation.Role;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Table(name = "auth_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "auth_user_id")
+    private int id;
+
+    @Column(name = "first_name")
     @NotNull
     @NotBlank
-    private String username;
+    private String name;
 
+    @Column(name = "last_name")
+    @NotNull
+    @NotBlank
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
     @NotNull
     @NotBlank
     private String passwordHash;
 
+    @Column(name = "mobile")
+    private String mobile;
+
+    @Column(name = "status")
+    private String status;
+
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    public User(String username, String password) {
-        this.username = username;
-        this.passwordHash=encoder.encode(password);;
+    public User(@NotNull @NotBlank String name, @NotNull @NotBlank String lastName, String email, @NotNull @NotBlank String passwordHash, String mobile, String status, Set<Role> roles) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.mobile = mobile;
+        this.status = status;
+        this.roles = roles;
     }
 
-    public boolean isMatchingPassword(String password) {
-        return encoder.matches(password, passwordHash);
+    public User() {
     }
 
-    public User(){
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "auth_user_role", joinColumns = @JoinColumn(name = "auth_user_id"), inverseJoinColumns = @JoinColumn(name = "auth_role_id"))
+    private Set<Role> roles;
 
-    }
-    public String getUsername() {
-        return username;
+    public int getId() {
+        return id;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setId(int id) {
+        this.id = id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
 
     public String getPasswordHash() {
         return passwordHash;
@@ -46,4 +135,6 @@ public class User {
     public void setPassword(String password) {
         this.passwordHash = passwordHash;
     }
+
+
 }
