ec8b3eca4735f7de5c6484355951e3c9fad74145
amukhjee <amukhjee1@gmail.com>
2020-05-17 12:40:18 -0500

    allows users to register and login. Acess to pages is still not done yet

--- src/main/java/org/launchcode/Competrack/AuthenticationFilter.java
+++ src/main/java/org/launchcode/Competrack/AuthenticationFilter.java
@@ -1,4 +1,6 @@
 package org.launchcode.Competrack;
 
-public class AuthenticationFilter {
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
 }
