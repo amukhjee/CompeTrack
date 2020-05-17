ab69068eec7f569f584a72d92de30a3d0273fddc
amukhjee <amukhjee1@gmail.com>
2020-05-17 13:10:19 -0500

    sucessfully login, register, and access control are working. yayyyyyyyyyyy

--- src/main/java/org/launchcode/Competrack/AuthenticationFilter.java
+++ src/main/java/org/launchcode/Competrack/AuthenticationFilter.java
@@ -1,6 +1,54 @@
 package org.launchcode.Competrack;
 
+import org.launchcode.Competrack.controllers.AuthenticationController;
+import org.launchcode.Competrack.data.UserRepository;
+import org.launchcode.Competrack.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 public class AuthenticationFilter extends HandlerInterceptorAdapter {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
+
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException {
+
+        if (isWhitelisted(request.getRequestURI())) {
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        // The user is logged in
+        if (user != null) {
+            return true;
+        }
+
+        // The user is NOT logged in
+        response.sendRedirect("/login");
+        return false;
+    }
 }
